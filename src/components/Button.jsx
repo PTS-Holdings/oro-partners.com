21192b1493ba201a892500f920aa95fa50ff08e8
Omar Sakr <o.sakr@ptscorp-eg.com>
2023-04-05 15:12:10 +0200

    style(Button.jsx): add arrow icon to button and adjust spacing with flex and gap properties

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 function Button({ children }) {
 	return <button
-		className="py-3 px-5 bg-custom-orange-basic body-medium-bold text-white rounded-full hover:bg-custom-orange-light">{children}
-		<span className="inline-block ml-5 ">
+		className="py-3 px-5 bg-custom-orange-basic body-medium-bold text-white rounded-full hover:bg-custom-orange-light flex items-center gap-5 hover:gap-8 transition-all">{children}
+		<span className="inline-block">
 			<Image src={"/assets/arrow-right.svg"} width={20} height={20} alt="arrow-right" />
 		</span>
 	</button>;
