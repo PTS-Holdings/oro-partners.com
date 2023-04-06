6db16eac5149c5d6168eef03ccb2a6de01b92d3e
Omar Sakr <o.sakr@ptscorp-eg.com>
2023-04-06 14:29:06 +0200

    feat(about): add TeamSection component to AboutUs page

---
+++ src/components/aboutPage/TeamSection.jsx
@@ -0,0 +1,17 @@
+export default function TeamSection() {
+    return (
+        <div className="text-center py-16 bg-[url('/assets/team-BG.png')] text-white bg-cover bg-center md:bg-top">
+            <div className="container flex flex-col items-center ">
+                <h2 className="md:header2 body-large-bold mb-6">Our <span className="text-custom-orange-basic">team</span></h2>
+                <p className="body-medium-regular body-small-regular md:w-1/2">
+                    Our team consists of experienced investors and entrepreneurs who
+                    bring a wealth of knowledge and expertise to our portfolio
+                    companies.Our investment professionals have a proven track
+                    record of success in the venture capital industry and are
+                    passionate about working with entrepreneurs to help them
+                    succeed.
+                </p>
+            </div>
+        </div>
+    );
+}
--- src/pages/about.js
+++ src/pages/about.js
@@ -1,4 +1,5 @@
 import AboutSection from "@/components/aboutPage/AboutSection";
+import TeamSection from "@/components/aboutPage/TeamSection";
 import Head from "next/head";
 
 export default function AboutUs() {
@@ -9,6 +10,7 @@ export default function AboutUs() {
 			</Head>
 
 			<AboutSection />
+			<TeamSection />
 		</>
 	);
 }
