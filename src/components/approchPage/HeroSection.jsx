a416ee0ba3f6f7d03225503616c4ef69ebf68857
Omar Sakr <o.sakr@ptscorp-eg.com>
2023-04-06 13:19:22 +0200

    style(HeroSection.jsx): format code and add missing semicolon feat(HeroSection.jsx): add line breaks and improve readability of the code

--- src/components/approchPage/HeroSection.jsx
+++ src/components/approchPage/HeroSection.jsx
@@ -1,15 +1,21 @@
-import Image from "next/image"
+import Image from "next/image";
 
 export default function HeroSection() {
     return (
-        <div className="flex lg:justify-between flex-col lg:flex-row items-center justify-center text-center lg:text-left gap-y-8 pb-6">
+        <div className="flex flex-col items-center justify-center gap-y-8 pb-6 text-center lg:flex-row lg:justify-between lg:text-left">
             <h1 className="header4 lg:header1 lg:w-1/2 ">
-                <span className="text-custom-orange-basic"> Our strategy </span>is centered on early-stage startups in the technology and consumer sectors.
+                <span className="text-custom-orange-basic"> Our strategy </span>
+                is centered on early-stage startups in the technology and
+                consumer sectors.
             </h1>
             <div>
-
-                <Image src={"/assets/approach-hero.svg"} width={400} height={383} alt={"hero Image"} unoptimized />
+                <Image
+                    src={"/assets/approach-hero.svg"}
+                    width={400}
+                    height={383}
+                    alt={"hero Image"}
+                />
             </div>
         </div>
-    )
+    );
 }
