89f4e584573f6333fb5e9b6194b33cfc741216bb
Omar Sakr <o.sakr@ptscorp-eg.com>
2023-04-08 17:34:04 +0200

    feat(toastfiy): add toastfiy container to main app

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,9 +1,12 @@
 import Layout from "@/layout";
 import "@/styles/globals.css";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 export default function App({ Component, pageProps }) {
 	return (
 		<Layout>
+			<ToastContainer />
 			<Component {...pageProps} />;
 		</Layout>
 	);
